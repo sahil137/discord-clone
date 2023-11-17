8f428c32a042f2d80c9e09b7f30b6ea3552e858c
Sahil Saini <sahilsaini137@gmail.com>
2023-11-17 21:27:08 +0530

    add shadcdn and setup folder structure

---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className='text-3xl text-blue-400'>
+      Discord Clone
+      <Button>
+        This is a button
+      </Button>
+    </div>
+  )
+}
